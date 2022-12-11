b60bdafc4362e6849fb0ce90f27cba7c3374d7cf
Carson Richter <carson.ri@protonmail.com>
2022-12-10 23:23:19 -0700

    sidebar name changed to side

--- app/components/Layouts/AppLayout.tsx
+++ app/components/Layouts/AppLayout.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import Footer from "../Footer";
 import NavBar from "../NavBar";
-import Sidebar from "../SideBar";
+import Side from "../Side";
+
 
 export default function AppLayout({children}: any) {
     const [isOpen, setIsOpen] = useState(false)
@@ -13,8 +14,7 @@ export default function AppLayout({children}: any) {
         <div>
             <div className="flex flex-col min-h-screen lg:flex lg:flex-row px-4 w-full mx-auto text-stone-300/60">
                 <NavBar toggle={toggle} isOpen={isOpen}/>
-                <Sidebar toggle={toggle} isOpen={isOpen}/>
-
+                <Side toggle={toggle} isOpen={isOpen}/>
                 <div className="w-full overflow-auto px-1">
                     {children}
                 </div>
--- app/components/Side.tsx
+++ app/components/Side.tsx
@@ -8,9 +8,9 @@ export const loader = async ( {request}: any ) => {
 
 }
 
-export default function Sidebar({toggle, isOpen}: any) {
+export default function Side({toggle, isOpen}: any) {
     const data = useLoaderData()
-    // console.log('sidebar data ->', data)
+    // console.log('side data ->', data)
 
     const links = [
         {title: 'Quotes', route: '/quotes', icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
