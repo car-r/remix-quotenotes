a9bd9fe902d0878566655a4c98d305be72ff19a9
Carson Richter <carson.ri@protonmail.com>
2023-01-01 15:45:15 -0700

    quotenotes index routes

---
+++ app/components/Buttons/EditNoteBtn.tsx
@@ -0,0 +1,16 @@
+import { NavLink } from "@remix-run/react";
+
+export default function EditBookBtn({data}: any) {
+
+    return (
+        <>
+            <NavLink to={`/notes/${data.data.id}/edit`} 
+            className={({ isActive }) =>
+            `text-xs xs:text-sm md:text-base px-3 py-2 text-stone-300 rounded text-center cursor-pointer border-2 border-stone-600 bg-transparent transition-all hover:ease-in-out hover:bg-stone-300/10 hover:border-stone-400 ${isActive ? "bg-stone-300/10 border-stone-400 " : ""}`
+            }
+            >  
+                {'Edit Note'}
+            </NavLink>
+        </>
+    )
+}
--- app/routes/notes/index.tsx
+++ app/routes/notes/index.tsx
@@ -1,12 +1,72 @@
-import { Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+import PageTitle from "~/components/PageTitle";
+import { prisma } from "~/db.server";
+import { requireUserId } from "~/session.server";
+import SectionTitle from "~/components/SectionTitle";
 
-export default function NoteIndexPage() {
-  return (
-    <p>
-      No note selected. Select a note on the left, or{" "}
-      <Link to="new" className="text-blue-500 underline">
-        create a new note.
-      </Link>
-    </p>
-  );
+export const loader = async ({request}: any) => {
+    const userId = await requireUserId(request);
+    const data = await prisma.user.findUnique({
+        where: {id: userId},
+        include: {
+            book: {
+                include: {
+                    quoteNote: true
+                }
+            },
+            _count: {
+                select: {
+                    quoteNote: true
+                }
+            }
+        }
+    })
+    return data
+}
+
+export default function NoteIndex() {
+    const data = useLoaderData()
+    // const noteCount = data.length
+    const noteCount = data._count.quoteNote
+    console.log('quotenoteindex ->', data)
+    return (
+        <>
+            <div className="flex flex-col pt-6 md:pt-10 max-w-5xl">
+                    {noteCount > 0 ? 
+                        <PageTitle children={`${noteCount} Notes`}/>
+                        : 
+                        <PageTitle children={`Notes`}/>
+                    }
+                <div className="grid grid-cols-1 gap-4">
+                    {data.book.map((book: any) => (
+                        <div key={book.title} className="mb-16">
+                            <SectionTitle children={book.title}/>
+                            <div className="flex overflow-auto pb-6 snap-x scrollbar-thin scrollbar-track-stone-800 scrollbar-thumb-stone-700">
+                                <div className="flex md:flex md:flex-row gap-4 mx-1">
+                                    {book.quoteNote.map((note: any) => (  
+                                        <Link to={`/notes/${note.id}`} key={note.id} className="flex justify-center items-center w-44 h-24  border border-stone-500 rounded-sm p-3 hover:bg-stone-700 hover:text-white">
+                                            <p className=" line-clamp-3 text-sm">{note.body}</p>
+                                        </Link>  
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+    console.error(error);
+  
+    return (
+        <div className="flex flex-col pt-6 md:pt-10 max-w-5xl">
+            <PageTitle children={`Notes`}/>
+            <div className='flex flex-col max-w-xl justify-center py-10 px-6  border border-red-500 text-red-500 rounded-lg text-center'>
+                <p className="text-sm font-semibold tracking-wide">{`Looks like an error: ${error}`}</p>
+            </div>
+        </div>
+    );
 }
